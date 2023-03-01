7afea7162877f7635b5e7d24dd5c7af8a1f95260
Plusgenie Ltd <aladdin@plusgenie.com>
2023-03-01 21:45:29 +0000

    Make SQLite3 columns lowercases update order fields as adding 4 clientId
    Signed-off-by: Plusgenie Ltd <aladdin@plusgenie.com>

--- src/settings.py
+++ src/settings.py
@@ -1,9 +1,9 @@
 # actions
-REGISTERED_ACTIONS = ['RedisPubActionClient3', 'RedisPubActionClient1', 'RedisPubActionClient2']
+REGISTERED_ACTIONS = ['RedisPubActionClient1', 'RedisPubActionClient2', 'RedisPubActionClient3', 'RedisPubActionClient4']
 
 # events
 REGISTERED_EVENTS = ['WebhookReceived']
-
+3
 # links
-REGISTERED_LINKS = [('RedisPubActionClient1', 'WebhookReceived'), ('RedisPubActionClient2', 'WebhookReceived'), ('RedisPubActionClient3', 'WebhookReceived')]
+REGISTERED_LINKS = [('RedisPubActionClient1', 'WebhookReceived'), ('RedisPubActionClient2', 'WebhookReceived'), ('RedisPubActionClient3', 'WebhookReceived'), ('RedisPubActionClient4', 'WebhookReceived')]
 
